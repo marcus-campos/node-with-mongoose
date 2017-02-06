571626e15b1978b88ce6d4d4aeb1dbe66db6f181
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-02-06 18:05:49 -0200

    Criando primeira aplicação. Estruturando um projeto.

--- app.js
+++ app.js
@@ -4,95 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
-
-
-//mongoose connect
-var db = mongoose.connect('mongodb://localhost/library').connection;
-
-db.on('open', function () {
-    console.log('Everything is okay, mongoDB is connected');
-});
-
-db.on('error', function () {
-    console.log('Ops! Something went wrong, mongoDB is broken');
-});
-
-var person = mongoose.Schema({
-    name: {
-        firstName: String,
-        lastName: String
-    }
-});
-
-//Objeto virtual
-person.virtual('name.fullName').get(function () {
-   return this.name.firstName.concat(' ').concat(this.name.lastName);
-});
-
-var Person = mongoose.model('Person', person);
-
-Person.create({
-    name: {
-        firstName: 'Marcus',
-        lastName: 'Campos'
-    }
-}, function (err, person) {
-
-    if(err) {
-        console.log('Error person ->', err);
-        return;
-    }
-
-    console.log('Person data -> ', person);
-    console.log('Person Fullname -> ', person.name.fullName)
-});
-
-var company = mongoose.Schema({
-    name: String,
-    address: {
-        name: String,
-        number: Number,
-        city: String
-    },
-    date: {
-        type: Date,
-        required: true,
-        default: Date.now
-    }
-});
-
-var Company = mongoose.model('Company', company);
-
-Company.create({
-    name: 'PlugApps',
-    address: {
-        name: 'Address 1',
-        number: 112,
-        city: 'São Paulo'
-    },
-    date: new Date()
-}, function (err, company) {
-    if(err){
-        console.log('error');
-        return;
-    }
-
-    console.log('Created -> ', company)
-});
-
-
-
-
-
-
-
-
-
+var hbs = require('hbs');
+var connection = require('./models/index');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var person = require('./routes/person')
 
 var app = express();
 
@@ -108,8 +25,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Middleware
 app.use('/', index);
 app.use('/users', users);
+app.use('/person', person);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
