dec4671ad2392891c6067d761234eb210d7ce7b0
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-02-07 10:32:50 -0200

    FindById, FindOne, Find

--- routes/person.js
+++ routes/person.js
@@ -2,8 +2,41 @@ var express = require('express');
 var router = express.Router();
 var Person = require('./../models/Person')
 
+//Find All
 router.get('/', function (req, res) {
-    res.send('Hello');
+    Person.find({}, function (err, people) {
+        if(err) {
+            return;
+        }
+
+        res.send(people);
+    });
+});
+
+//Find One
+router.get('/:id', function (req, res) {
+
+    //Find by ID
+
+    Person.findById(req.params.id, function (err, person) {
+
+        if(err){
+            return;
+        }
+
+        res.send(person);
+    });
+
+    //Find One
+    /*Person.findOne({
+        _id: req.params.id
+    },function (err, person) {
+        if(err){
+            return;
+        }
+
+        res.send(person);
+    })*/
 });
 
 module.exports = router;
